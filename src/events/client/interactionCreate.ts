c3e139cbdf0db6765dafb7bc9f65a6851ae36d88
lrmn7 <lrmn.dev@gmail.com>
2023-09-13 22:46:09 +0700

    test: test

--- src/events/client/interactionCreate.ts
+++ src/events/client/interactionCreate.ts
@@ -2,12 +2,22 @@ import type { Interaction } from 'discord.js'
 import type { Lrmn } from '../../core/Lrmn.js'
 
 export const event = async (client: Lrmn, interaction: Interaction) => {
-	if (!interaction.isChatInputCommand() || !interaction.guild) return
+	if (interaction.isChatInputCommand() && interaction.guild) {
+		const settings = client.database.get(interaction.guild.id)
+		const translate = (translatable: string, replaceable: Record<string, string> = {}) =>
+			client.locales.translate(settings.locale, translatable, replaceable)
 
-	const settings = client.database.get(interaction.guild.id)
-	const translate = (translatable: string, replaceable: Record<string, string> = {}) =>
-		client.locales.translate(settings.locale, translatable, replaceable)
+		const command = client.commands.get(interaction.commandName, settings.locale)
 
-	const command = client.commands.get(interaction.commandName, settings.locale)
-	if (command) command.run({ client, interaction, translate, settings })
+		// Tambahkan penanganan jika perintah tidak ditemukan
+		if (command) {
+			// Jalankan perintah jika ditemukan
+			command.run({ client, interaction, translate, settings })
+		} else {
+			await interaction.reply({
+				content: translate('COMMAND_NOT_FOUND'), // Ganti dengan pesan yang sesuai
+				ephemeral: true // Ini akan membuat pesan hanya terlihat oleh pengguna yang mengirimkan perintah
+			})
+		}
+	}
 }
